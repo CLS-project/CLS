15c41d139e7391b4d2aea01ad4314dd9542e54e4
CLS-project <163777038+CLS-project@users.noreply.github.com>
2024-09-04 12:37:14 +0200

    Update projects.ts
    test

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -35,22 +35,6 @@ Developed a mock package used internally for testing and debugging purposes, usi
 Followed best-practices and guidelines, including writing Unit and E2E tests.
 Worked on a dynamic multicultural team that follows an Agile/Scrum methodology.
 Shared knowledge and learned with teammates through pair programming.`
-  },
-  {
-    name: 'IQA - Sistemas de Gestão, Lda',
-    image: iqaImg,
-    url: 'https://www.myagir.com/en/',
-    role: 'Full Stack Developer (Lead, Architect)',
-    date: '03/2011 - 05/2021',
-    description: `Developed AGIR ®, a Quality Management Software (QMS), Process Management (BPM) and Document control software. Worked on the platform since 2011: From ASP.NET, SQL Server, Web API, .NET WinForms or Java to Windows Services and Office Add-Ins, (...).
-Created specific features that give this platform a unique edge in flexibility - features like an intuitive drag & drop Form Designer, a custom JavaScript framework (based on jQuery, later Vue) and a business logic assistant (workflow, form logic) mean that it is able to adapt to every customer need, providing infinite possibilities.
-Worked close to the project manager and customers to ensure that every product meets requirements in a versatile, modern and performant way.
-Helped customers on every step - Configuration, deployment, troubleshooting, etc.
-Developed and provided APIs, tools and other custom mechanisms in order to adapt and integrate with existing solutions, databases and products.
-One of those tools/integrations included creating an extremely optimised data matrix code scanner from TIFF files in Python, using tools like numpy, CV2, Wand, and more.
-Developed a solid foundation for the company's next project, Wisibi, using Quasar Framework (Vue.js) and .NET Core Web API, following all the best practices.
-Managed the team's Trello, GitHub, Jenkins and other services.
-Shared knowledge with teammates through training.`
   }
 ]
 
